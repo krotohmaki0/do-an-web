3f002e12b5e98fffb84b52ef2a3c5959bbed37c5
Kro <krotohmaki@gmail.com>
2021-11-03 13:05:03 +0700

    Tao form-dang-nhap.html

--- www/main.js
+++ www/main.js
@@ -1,4 +1,7 @@
 $(document).ready((e) => {
+	//////////////////////////////
+	////////// function //////////
+	//////////////////////////////
 	$.get("http://localhost:8080/admin/getData.php", (data, status) => {  
 		console.dir(data);
 	}, "json");
